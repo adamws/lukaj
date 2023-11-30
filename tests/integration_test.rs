a3e2f52d115abd556a882986e9ed3f7ca39f0dda
adamws <adamws@users.noreply.github.com>
2023-11-30 20:51:21 +0100

    Compare images without imagemagick
    Reduced scale factor for test case because it will cause problems on
    windows runner on github actions because it is limited to 1024x728 and
    the full canvas when doing zoom 5x won't fit.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,3 +1,7 @@
+use dssim_core::{Dssim, DssimImage, Val};
+use image::imageops::FilterType;
+use image::DynamicImage;
+use rgb::*;
 use rstest::rstest;
 use std::env;
 use std::io;
@@ -34,25 +38,58 @@ fn wrap_with_xvfb(command: &mut Command) -> io::Result<Child> {
     }
 }
 
-fn compare_images(actual: &String, expected: &String) -> f64 {
-    let output = Command::new("magick")
-        .arg("compare")
-        .arg(actual)
-        .arg(expected)
-        .args(&["-metric", "DSSIM", "null:"])
-        .output()
-        .expect("failed to execute process");
+fn image(path: &String) -> Result<DynamicImage, String> {
+    let image = image::open(path).map_err(|e| e.to_string())?;
+    Ok(image)
+}
 
-    String::from_utf8(output.stderr)
-        .unwrap()
-        .parse::<f64>()
-        .unwrap()
+fn dssim_image(img: DynamicImage, dssim: &Dssim) -> Result<DssimImage<f32>, String> {
+    let width = usize::try_from(img.width()).map_err(|e| e.to_string())?;
+    let height = usize::try_from(img.height()).map_err(|e| e.to_string())?;
+
+    let raw = img.as_bytes().as_rgb();
+    match dssim.create_image_rgb(raw, width, height) {
+        Some(img) => Ok(img),
+        None => Err(format!("Failed to create DSSIM image")),
+    }
+}
+
+fn percentage_diff(u1: u32, u2: u32) -> f64 {
+    let v1 = u1 as f64;
+    let v2 = u2 as f64;
+    (v1 - v2).abs() / ((v1 + v2) / 2.0) * 100.0
+}
+
+fn compare_images(actual: &String, expected: &String) -> Result<Val, String> {
+    let dssim = Dssim::new();
+
+    let img1 = image(actual)?;
+    let mut img2 = image(expected)?;
+
+    if img2.width() != img1.width() || img2.height() != img1.height() {
+        // due to SVG backends differences, resulting image size might be slightly
+        // different than reference (reference generated only for one backend)
+        // let this slide if difference is less than 0.5% and force resize of result
+        // (because same size images are required by dssim comparator)
+        let width_diff = percentage_diff(img2.width(), img1.width());
+        let height_diff = percentage_diff(img2.height(), img1.height());
+        if width_diff < 0.5 && height_diff < 0.5 {
+            img2 = img2.resize_exact(img1.width(), img1.height(), FilterType::Nearest);
+        } else {
+            return Err(format!("Images size difference exceeds acceptable limit"));
+        }
+    }
+
+    let dssim1 = dssim_image(img1, &dssim)?;
+    let dssim2 = dssim_image(img2, &dssim)?;
+
+    Ok(dssim.compare(&dssim1, dssim2).0)
 }
 
 #[rstest]
 #[case("rsvg-with-cairo")]
 #[case("usvg-with-skia")]
-fn run_diff(#[case] backend: String) {
+fn run_diff(#[case] backend: String) -> Result<(), String> {
     let base: &'static str = env!("CARGO_MANIFEST_DIR");
     let executable: &'static str = env!("CARGO_BIN_EXE_lukaj");
     let tmpdir: &'static str = env!("CARGO_TARGET_TMPDIR");
@@ -60,31 +97,32 @@ fn run_diff(#[case] backend: String) {
     let screenshot_name = format!("{}.bmp", backend);
     let result = format!("{}/{}", tmpdir, screenshot_name);
     let reference = format!("{}/tests/references/run_diff.bmp", base);
-    let threshold = 0.1;
+    let threshold = 0.07;
 
     let mut command = Command::new(executable);
     command
         .env("CARGO_TARGET_TMPDIR", tmpdir)
         .env("TEST_OUTPUT_FILENAME", screenshot_name)
         .args(&[
-            "-s5",
+            "-s2",
             "--backend",
             &backend,
             "tests/images/arcs01.svg",
             "tests/images/arcs01_2.svg",
         ]);
 
-    let wrapped = wrap_with_xvfb(&mut command).expect("Failed to create command");
+    let wrapped = wrap_with_xvfb(&mut command).map_err(|e| e.to_string())?;
 
-    let output = wrapped.wait_with_output().unwrap();
+    let output = wrapped.wait_with_output().map_err(|e| e.to_string())?;
     assert!(output.status.success());
     assert!(Path::new(&result).exists());
 
-    let difference = compare_images(&result, &reference);
+    let difference = compare_images(&result, &reference)?;
     assert!(
         difference < threshold,
         "Diffirence metric {} above threshold {}",
         difference,
         threshold
     );
+    Ok(())
 }
